ddce4799e8cc88a8bc71dd32f2c7fb1da1cd9fff
aminsyat727 <aminsyatir.ms@gmail.com>
2024-11-08 22:27:55 +0800

    added

--- practicepython/22-ReadFromFile.py
+++ practicepython/22-ReadFromFile.py
@@ -12,6 +12,7 @@ def category_counter(dict, line):
 
 
 if __name__ == "__main__":
+    import re
     counter = {}
     with open('nameslist.txt', 'r') as open_file:
         line = open_file.readline()
@@ -20,4 +21,12 @@ if __name__ == "__main__":
             counter = category_counter(counter, line)
             line = open_file.readline()
     print(counter)
+    harder_counter = {}
+    with open('Training_01.txt', 'r') as open_file:
+        line = open_file.readline()
+        while line:
+            line = line.replace("\n", "").split("/", -1)[2]
+            harder_counter = category_counter(harder_counter, line)
+            line = open_file.readline()
+    print(harder_counter)
 
